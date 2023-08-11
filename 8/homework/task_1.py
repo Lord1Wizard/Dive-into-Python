0557728f9368806e7e00b9c1888276355c3b6275
Lord1Wizard <lord_wizard@mail.ru>
2023-08-11 18:50:04 +0300

    Add HomeWork 8

--- 8/homework/task_1.py
+++ 8/homework/task_1.py
@@ -7,29 +7,24 @@ import csv
 import json
 import os
 import pickle
-from pprint import pprint
 
 my_dict={}
 def directory_overviewer(path: str):
     """Функция рекурсивно обходит все директории и файлы и заполняет словарь значениями"""
     size = 0
     global my_dict
-    # print('-----')
-    # if os.path.exists(path) :
     if os.path.isdir(path):
         for i in os.listdir(path):
-            # print(path+i)
             size += directory_overviewer(path+'\\'+i)
         my_dict[path] = [path.split('\\')[-1],'isDir',size,path.split('\\')[-2]]
         return size
     else:
         size = os.path.getsize(path)
-        # print(path, size)
         my_dict[path] = [path.split('\\')[-1],'isFile',size,path.split('\\')[-2]]
         return size
 
 def save_csv(dict:{}):
-    """апись словаря в csv"""
+    """Запись словаря в csv"""
     with open('my_file.csv', 'w', newline='', encoding='utf-8') as f_csv:
         csv_write = csv.writer(f_csv, delimiter=';')
         line = ['path', 'name', 'type', 'size', 'parent']
@@ -40,12 +35,12 @@ def save_csv(dict:{}):
         csv_write.writerows(all_data)
 
 def save_picle(dict:{}):
-    """апись словаря в picle"""
+    """Запись словаря в picle"""
     with open('my_file.pickle', 'wb') as f_pickle:
         pickle.dump(dict, f_pickle)
 
 def save_json(dict:{}):
-    """апись словаря в json"""
+    """Запись словаря в json"""
     with open('my_file.json', 'w', encoding='utf-8') as f_json:
         json.dump(dict, f_json, indent=2)
 
